14b40f277d9f8edbef2242fa1d703000ee7fe273
Morierti <rodrigoramos.rosa2@gmail.com>
2016-03-06 19:11:12 +0000

    Cleanup

--- readernotes/src/database/BookXML.java
+++ readernotes/src/database/BookXML.java
@@ -1,5 +1,40 @@
 package readernotes.src.database;
 
-public class BookXML {
+import org.jdom2.Attribute;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.output.Format;
+import readernotes.src.core.Book;
 
+public class BookXML {
+	private Book _bookObject;
+	private Document _xmlDocument;
+
+	public BookXML(Book bookObject) {
+		setBookObject(bookObject);
+	}
+
+	public void setBookObject(Book bookObject) {
+		_bookObject = bookObject;
+	}
+
+	public Book getBookObject() {
+		return _bookObject;
+	}
+
+	public Element createTitleElement() {
+	}
+
+	public Element createAuthorElement() {
+	}
+
+	public Element createSinopseElement() {
+	}
+
+	public void buildDocument() {
+	}
+
+	public Document getDocument() {
+		return _xmlDocument;
+	}
 }
