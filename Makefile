91658165da16e6ff30998b75b645f403bda89fa5
Morierti <rodrigoramos.rosa2@gmail.com>
2016-03-18 09:00:09 +0000

    New Lib + BookXML

--- Makefile
+++ Makefile
@@ -1,13 +1,15 @@
 #Makefile
 CORE = readernotes/src/core
 EXCEPTIONS = readernotes/src/exceptions
+DATABASE = readernotes/src/database
 CORE_TESTS = readernotes/test/core
 JUNIT_PATH=libs/junit-4.12.jar
 HAMCREST=libs/java-hamcrest-2.0.0.0.jar
+JDOM=libs/jdom-2.0.6/jdom-2.0.6.jar
 JUNIT_EXEC=org.junit.runner.JUnitCore
 
 all:
-	javac -cp $(JUNIT_PATH) `find $(CORE) $(EXCEPTIONS) $(CORE_TESTS) \
+	javac -cp $(JUNIT_PATH):$(JDOM) `find $(CORE) $(EXCEPTIONS) $(CORE_TESTS) $(DATABASE) \
     -name *.java`
 
 run:
@@ -22,4 +24,5 @@ test:
 clean:
 	rm readernotes/src/core/*.class
 	rm readernotes/src/exceptions/*.class
+	rm readernotes/src/database/*.class
 	rm readernotes/test/core/*.class
--- readernotes/src/database/BookXML.java
+++ readernotes/src/database/BookXML.java
@@ -1,9 +1,11 @@
 package readernotes.src.database;
 
+import java.io.IOException;
 import org.jdom2.Attribute;
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.output.Format;
+import org.jdom2.output.XMLOutputter;
 import readernotes.src.core.Book;
 
 public class BookXML {
@@ -23,18 +25,53 @@ public class BookXML {
 	}
 
 	public Element createTitleElement() {
+		Element titleElement = new Element("Title");
+		titleElement.setText(_bookObject.getTitle());
+		return titleElement;
 	}
 
 	public Element createAuthorElement() {
+		Element authorElement = new Element("Author");
+		titleElement.setText(_bookObject.getAuthor());
+		return authorElement;
 	}
 
 	public Element createSinopseElement() {
+		Element sinopseElement = new Element("Sinopse");
+		sinopseElement.setText(_bookObject.getSinopse());
+		return sinopseElement;
 	}
 
-	public void buildDocument() {
+	public void buidDocument()
+	throws
+	IOException {
+		Element bookElement = new Element("Book");
+		Document bookDocument = new Document(bookElement);
+		Element title = createTitleElement();
+		Element author = createAuthorElement();
+		Element sinopse = createSinopseElement();
+		bookDocument.getRootElement().addContent(title);
+		bookDocument.getRootElement().addContent(author);
+		bookDocument.getRootElement().addContent(sinopse);
+
+		//TEST
+		XMLOutputter xmlOutput = new XMLOutputter();
+		xmlOutput.setFormat(Format.getPrettyFormat());
+		xmlOutput.output(bookDocument, System.out);
 	}
 
 	public Document getDocument() {
 		return _xmlDocument;
+	}
+
+	public static void main(String[] args) {
+		try {	
+			Book newBook = new Book("Book Title", "Author Title");
+			newBook.setSinopse("Book Sinopse or something");
+			BookXML book = new BookXML(newBook);
+			book.buildDocument();
+		} catch (IOException exception) {
+			exception.printStackTrace();
+		}
 	}
 }
