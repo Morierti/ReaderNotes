9e957b3ee7e581c670f1d61290b5c5c8de6887b2
Morierti <rodrigoramos.rosa2@gmail.com>
2016-04-20 14:56:20 +0100

    Complete working exceptions.
    Exceptions for Sintese and Book management are now complete.

--- readernotes/src/exceptions/EmptyAuthorException.java
+++ readernotes/src/exceptions/EmptyAuthorException.java
@@ -1,6 +1,10 @@
 package readernotes.src.exceptions;
 
 public class EmptyAuthorException extends Exception {
-
+    private static final String _errorMessage = "The Author can't be empty.";
+    
+    public String getMessage() {
+        return _errorMessage;
+    }
 
 }
--- readernotes/src/exceptions/EmptyTitleException.java
+++ readernotes/src/exceptions/EmptyTitleException.java
@@ -1,6 +1,10 @@
 package readernotes.src.exceptions;
 
 public class EmptyTitleException extends Exception {
-
+    private static final String _errorMessage = "The Title cannot be empty.";
+    
+    public String getMessage() {
+        return _errorMessage;
+    }
 
 }
