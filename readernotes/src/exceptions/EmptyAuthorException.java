ab0c68bf4642496da06aad78cab4caa30493c5b6
Morierti <rodrigoramos.rosa2@gmail.com>
2016-05-20 21:19:18 +0100

    Closes #24
    Exceptions package refactored.

--- readernotes/src/exceptions/EmptyAuthorException.java
+++ readernotes/src/exceptions/EmptyAuthorException.java
@@ -1,8 +1,8 @@
 package readernotes.src.exceptions;
 
 public class EmptyAuthorException extends Exception {
-    private static final String _errorMessage = "The Author can't be empty.";
-    
+    private static final String _errorMessage = "Please insert an author.";
+
     public String getMessage() {
         return _errorMessage;
     }
--- readernotes/src/exceptions/EmptyTitleException.java
+++ readernotes/src/exceptions/EmptyTitleException.java
@@ -1,8 +1,8 @@
 package readernotes.src.exceptions;
 
 public class EmptyTitleException extends Exception {
-    private static final String _errorMessage = "The Title cannot be empty.";
-    
+    private static final String _errorMessage = "Please insert a title.";
+
     public String getMessage() {
         return _errorMessage;
     }
