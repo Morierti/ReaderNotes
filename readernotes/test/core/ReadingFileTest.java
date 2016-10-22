ce88eda39beed482943b3d0f73c34dba6de251ed
rodrigorar <rodrigoramos.rosa2@gmail.com>
2016-10-22 12:11:51 +0100

    Closes #46
    - Unit test for the ReadingFiles class is now done with
    16 test cases.

--- readernotes/test/core/ReadingFileTest.java
+++ readernotes/test/core/ReadingFileTest.java
@@ -19,7 +19,10 @@ Copyright (C) 2016  Rodrigo Ramos Rosa
 package readernotes.test.core;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 import org.junit.Test;
 import org.junit.Before;
 import org.junit.After;
@@ -27,13 +30,30 @@ import readernotes.src.core.ReadingFile;
 import readernotes.src.exceptions.EmptyTitleException;
 
 public class ReadingFileTest {
-    //TOKENS
 
+    //TOKENS
     private static final String RF_TITLE = "Chapter I - Adventure";
+    private static final String RF_NEW_TITLE = "Chapter II - Adventure";
     private static final String RF_BOOK_TITLE = "Adventurer";
+    private static final String RF_NEW_BOOK_TITLE = "Guitarman";
     private static final String RF_SUBJECT = "Store of the Amidas Adventure";
     private static final String RF_CONTENT = "This is the story of...";
     private static final String DEFAULT_VALUE = "DEFAULT";
+    private ReadingFile defaultFile;
+
+    @Before
+    public void prepareForTest() {
+        try {
+            defaultFile = new ReadingFile(RF_TITLE, RF_BOOK_TITLE);
+        } catch (EmptyTitleException exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+
+    @After
+    public void tearDown() {
+        defaultFile = null;
+    }
 
     @Test
     public void ReadingFileCreationSuccess()
@@ -55,72 +75,157 @@ public class ReadingFileTest {
     public void ReadingFileCreationSuccess2()
     throws
     EmptyTitleException {
-        ReadingFile testFile = new ReadingFile(this.RF_TITLE, this.RF_BOOK_TITLE);
 
-        assertNotNull(testFile);
-        assertEquals(this.RF_TITLE, testFile.getTitle());
-        assertEquals(this.RF_BOOK_TITLE, testFile.getBookTitle());
-        assertEquals(this.DEFAULT_VALUE, testFile.getSubject());
-        assertEquals(this.DEFAULT_VALUE, testFile.getContent());
+        assertNotNull(defaultFile);
+        assertEquals(this.RF_TITLE, defaultFile.getTitle());
+        assertEquals(this.RF_BOOK_TITLE, defaultFile.getBookTitle());
+        assertEquals(this.DEFAULT_VALUE, defaultFile.getSubject());
+        assertEquals(this.DEFAULT_VALUE, defaultFile.getContent());
 
     }
 
     @Test
     public void TitleUpdate()
-    throws {
+    throws
+    EmptyTitleException {
+        ReadingFile fileTest = new ReadingFile(this.RF_TITLE, this.RF_BOOK_TITLE);
 
+        assertNotNull(fileTest);
+        assertEquals(this.RF_TITLE, fileTest.getTitle());
+        assertEquals(this.RF_BOOK_TITLE, fileTest.getBookTitle());
+
+        fileTest.setTitle(this.RF_NEW_TITLE);
+
+        assertEquals(this.RF_NEW_TITLE, fileTest.getTitle());
     }
 
     @Test (expected = EmptyTitleException.class)
     public void nullTitleUpdate()
     throws
     EmptyTitleException {
+        ReadingFile fileTest = new ReadingFile(null, this.RF_BOOK_TITLE);
 
+        assertNotNull(fileTest);
     }
 
     @Test (expected = EmptyTitleException.class)
     public void emptyTitleUpdate()
     throws
     EmptyTitleException {
+        ReadingFile fileTest = new ReadingFile("", this.RF_BOOK_TITLE);
 
+        assertNotNull(fileTest);
     }
 
     @Test
     public void BookTitleUpdate()
     throws
     EmptyTitleException {
+        ReadingFile fileTest = new ReadingFile(this.RF_TITLE, this.RF_BOOK_TITLE);
 
+        assertNotNull(this.RF_TITLE, fileTest.getBookTitle());
+
+        fileTest.setBookTitle(this.RF_NEW_BOOK_TITLE);
+
+        assertEquals(this.RF_NEW_BOOK_TITLE, fileTest.getBookTitle());
     }
 
     @Test (expected = EmptyTitleException.class)
     public void nullBookTitleUpdate()
     throws
     EmptyTitleException {
+        ReadingFile testFile = new ReadingFile(RF_TITLE, null);
 
+        assertNotNull(testFile);
     }
 
     @Test (expected = EmptyTitleException.class)
-    public void emptyTitleUpdate()
+    public void emptyBookTitleUpdate()
     throws
     EmptyTitleException {
+        ReadingFile testFile = new ReadingFile(RF_TITLE, "");
 
+        assertNotNull(testFile);
     }
 
     @Test
     public void subjectUpdate()
     throws
     EmptyTitleException {
+        ReadingFile testFile = new ReadingFile(RF_TITLE, RF_BOOK_TITLE);
 
+        testFile.setSubject(RF_SUBJECT);
+
+        assertEquals(testFile.getSubject(), RF_SUBJECT);
+    }
+
+    @Test
+    public void nullSubjectUpdate()
+    throws
+    EmptyTitleException {
+        ReadingFile testFile = new ReadingFile(RF_TITLE, RF_BOOK_TITLE);
+
+        testFile.setSubject(null);
+        assertNull(testFile.getSubject());
+    }
+
+    @Test
+    public void emptySubjectUpdate()
+    throws
+    EmptyTitleException {
+        ReadingFile testFile = new ReadingFile(RF_TITLE, RF_BOOK_TITLE);
+
+        testFile.setSubject("");
+        assertEquals("", testFile.getSubject());
     }
 
     @Test
     public void contentUpdate()
     throws
     EmptyTitleException {
+        ReadingFile testFile = new ReadingFile(RF_TITLE, RF_BOOK_TITLE);
 
+        testFile.setContent(RF_CONTENT);
+        assertEquals(RF_CONTENT, testFile.getContent());
     }
 
     @Test
+    public void nullContentUpdate()
+    throws
+    EmptyTitleException {
+        ReadingFile testFile = new ReadingFile(RF_TITLE, RF_BOOK_TITLE);
 
+        testFile.setContent(null);
+        assertNull(testFile.getContent());
+    }
+
+    @Test
+    public void emptyContentUpdate()
+    throws
+    EmptyTitleException {
+        ReadingFile testFile = new ReadingFile(RF_TITLE, RF_BOOK_TITLE);
+
+        testFile.setContent("");
+        assertEquals("", testFile.getContent());
+    }
+
+    @Test
+    public void nullVerifyIfEmptyTest()
+    throws
+    EmptyTitleException {
+        ReadingFile testFile = new ReadingFile(RF_TITLE, RF_BOOK_TITLE);
+
+        assertTrue(testFile.verifyIfEmpty(null));
+        assertTrue(testFile.verifyIfEmpty(""));
+    }
+
+    @Test
+    public void notNullVerifyIfEmptyTest()
+    throws
+    EmptyTitleException {
+        ReadingFile testFile = new ReadingFile(RF_TITLE, RF_BOOK_TITLE);
+
+        assertFalse(testFile.verifyIfEmpty(RF_TITLE));
+    }
 
 }
