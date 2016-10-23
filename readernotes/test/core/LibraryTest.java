c657630ab294b300ff916845bfceabf3a4e7ee48
rodrigorar <rodrigoramos.rosa2@gmail.com>
2016-10-23 01:10:23 +0100

    Test Corrections

--- readernotes/test/core/LibraryTest.java
+++ readernotes/test/core/LibraryTest.java
@@ -10,29 +10,39 @@ import readernotes.src.core.Library;
 import readernotes.src.core.ReadingFile;
 import readernotes.src.core.Book;
 import readernotes.src.exceptions.DoubleEntryException;
+import readernotes.src.exceptions.EmptyTitleException;
+import readernotes.src.exceptions.EmptyAuthorException;
+import readernotes.src.exceptions.InexistentBookException;
+import readernotes.src.exceptions.InexistentReadingFileException;
 
 public class LibraryTest {
     //TOKENS
-    private static final String BOOK_TITLE = "Adventurer"
+    private static final String BOOK_TITLE = "Adventurer";
     private static final String READINGFILE_TITLE = "Cp. 1 The Beginning";
     private static final String BOOK_AUTHOR = "John";
     private static final String BOOK_ISBN = "123-456-789";
     private static final String BOOK_SUBJECT = "Adventure";
-    private Library _testLibrary;
+    private Library _library;
     private ReadingFile _testFile;
     private Book _testBook;
 
     @Before
     public void setup() {
-        _testLibrary = Library.getInstance();
-        _testBook = new Book(BOOK_TITLE, BOOK_AUTHOR);
-        _testBook.setISBN(BOOK_ISBN);
-        _testBook.setSubject(BOOK_SUBJECT);
+        try {
+            _library = Library.getInstance();
+            _testBook = new Book(BOOK_TITLE, BOOK_AUTHOR);
+            _testBook.setISBN(BOOK_ISBN);
+            _testBook.setSubject(BOOK_SUBJECT);
 
-        _testFile = new ReadingFile(READINGFILE_TITLE, BOOK_TITLE);
+            _testFile = new ReadingFile(READINGFILE_TITLE, BOOK_TITLE);
 
-        _testLibrary.addBook(testBook);
-        _testLibrary.addReadingFile(testReadingFile);
+            _library.addBook(_testBook);
+            _library.addReadingFile(_testFile);
+        } catch (EmptyTitleException
+                | DoubleEntryException
+                | EmptyAuthorException exception) {
+            System.err.println(exception.getMessage());
+        }
     }
 
     @After
@@ -50,34 +60,45 @@ public class LibraryTest {
     }
 
     @Test
-    public void getBookTestByTitle() {
+    public void getBookTestByTitle()
+    throws
+    InexistentBookException {
         Book testBook = _library.getBook(BOOK_TITLE);
 
         assertEquals(testBook.getTitle(), BOOK_TITLE);
     }
 
     @Test
-    public void getReadingFileTest() {
+    public void getReadingFileTest()
+    throws
+    InexistentReadingFileException {
         ReadingFile testFile = _library.getReadingFile(READINGFILE_TITLE);
 
         assertEquals(testFile.getTitle(), READINGFILE_TITLE);
     }
 
     @Test (expected = DoubleEntryException.class)
-    public void addExistingBook() {
+    public void addExistingBook()
+    throws
+    DoubleEntryException,
+    EmptyTitleException,
+    EmptyAuthorException {
         Book newBook = new Book(BOOK_TITLE, BOOK_AUTHOR);
         _library.addBook(newBook);
     }
 
     @Test (expected = DoubleEntryException.class)
-    public void addExistingReadingFile() {
+    public void addExistingReadingFile()
+    throws
+    DoubleEntryException,
+    EmptyTitleException {
         ReadingFile newFile = new ReadingFile(READINGFILE_TITLE, BOOK_TITLE);
         _library.addReadingFile(newFile);
     }
 
     @Test
     public void matchesBookTitleID() {
-        assertTrue(_library.matchesBookTitleID(BOOK_TITLE, _testBook))
+        assertTrue(_library.matchesBookID(BOOK_TITLE, _testBook));
     }
 
     @Test
@@ -92,7 +113,7 @@ public class LibraryTest {
 
     @Test
     public void matchesBookSubject() {
-        assertTrue(_library.matchesBookSubject(BOOK_SUBJECT, _testBook));
+        assertTrue(_library.matchesBookID(BOOK_SUBJECT, _testBook));
     }
 
 }
